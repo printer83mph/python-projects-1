3c773d30a51440ee5e7128d5fc4bb7f362867b2c
Thomas Shaw <printer.83mph@gmail.com>
2016-11-01 16:11:55 -0400

    Add optimized bulls and cows

---
+++ cows_bulls_optimized.py
@@ -0,0 +1,41 @@
+# Cows and Bulls
+# Optimized
+
+import random
+
+def main():
+	num = []
+	while(len(num) < 4):
+		gen_num = random.randint(1,9)
+		works = True
+		for used_num in range(0,len(num)):
+			if(gen_num == num[used_num]):
+				works = False
+		if(works):
+			num.append(gen_num)
+	print(num)
+	won = False
+	while(won == False):
+		guess = list(raw_input("Guess?\n>>> "))
+		print(guess)
+		bulls = 0
+		cows = 0
+		# Check for bulls
+		for cur_num in range(0,4):
+			guess[cur_num] = int(guess[cur_num])
+			if(guess[cur_num] == num[cur_num]):
+				cows += 1
+			for check_num in range(0,4):
+				if(cur_num != check_num):
+					if(guess[cur_num] == num[check_num]):
+						bulls += 1
+						break
+		print(str(bulls) + " bulls")
+		print(str(cows) + " cows") 
+		if(guess == num):
+			won = True
+	print("You won!")
+				
+
+if(__name__ == "__main__"):
+	main()
